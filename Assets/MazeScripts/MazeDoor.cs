8a88f3983f73548b244f5cb20adbfd54a0dc006d
PoyoMain <CLC9512@GMAIL.COM>
2024-01-27 18:36:46 -0500

    Maze wall and door made

---
+++ Assets/MazeScripts/MazeDoor.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D), typeof(SpriteRenderer))]
+public class MazeDoor : MonoBehaviour
+{
+    private SpriteRenderer _sRend;
+    [SerializeField] private GameColor doorColor;
+
+    private void Awake()
+    {
+        _sRend = GetComponent<SpriteRenderer>();
+
+        _sRend.color = (doorColor) switch
+        {
+            GameColor.Red => Color.red,
+            GameColor.Blue => Color.blue,
+            GameColor.Green => Color.green,
+            _ => Color.white
+        };
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<bullet>(out _))
+        {
+            switch (collision.gameObject.tag)
+            {
+                case "Red":
+                    if (doorColor == GameColor.Red) Destroy(this.gameObject);
+                    break;
+
+                case "Blue":
+                    if (doorColor == GameColor.Blue) Destroy(this.gameObject);
+                    break;
+
+                case "Green":
+                    if (doorColor == GameColor.Green) Destroy(this.gameObject);
+                    break;
+            }
+        }
+    }
+}
+
+public enum GameColor
+{
+    Red,
+    Green,
+    Blue,
+}
---
+++ Assets/MazeScripts/MazeWall.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D), typeof(SpriteRenderer))]
+public class MazeWall : MonoBehaviour
+{
+    private SpriteRenderer _sRend;
+
+    private void Awake()
+    {
+        _sRend = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<bullet>(out _))
+        {
+            _sRend.color = (collision.gameObject.tag) switch
+            {
+                "Red" => Color.red,
+                "Blue" => Color.blue,
+                "Green" => Color.green,
+                _ => Color.white
+            };
+        }
+    }
+}
