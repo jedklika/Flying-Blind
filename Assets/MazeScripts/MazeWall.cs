5f2095d9e492787a01e97bca178c4a386b6114f0
PoyoMain <CLC9512@GMAIL.COM>
2024-01-28 01:17:03 -0500

    Maze Wall color

--- Assets/MazeScripts/MazeWall.cs
+++ Assets/MazeScripts/MazeWall.cs
@@ -5,24 +5,24 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D), typeof(SpriteRenderer))]
 public class MazeWall : MonoBehaviour
 {
-    private SpriteRenderer _sRend;
+    //private SpriteRenderer _sRend;
 
-    private void Awake()
-    {
-        _sRend = GetComponent<SpriteRenderer>();
-    }
+    //private void Awake()
+    //{
+    //    _sRend = GetComponent<SpriteRenderer>();
+    //}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.TryGetComponent<bullet>(out _))
         {
-            _sRend.color = (collision.gameObject.tag) switch
-            {
-                "Red" => Color.red,
-                "Blue" => Color.blue,
-                "Green" => Color.green,
-                _ => Color.white
-            };
+            //_sRend.color = (collision.gameObject.tag) switch
+            //{
+            //    "Red" => Color.red,
+            //    "Blue" => Color.blue,
+            //    "Green" => Color.green,
+            //    _ => Color.white
+            //};
 
             Destroy(collision.gameObject);
         }
