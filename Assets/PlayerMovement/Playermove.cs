b157b049ce0026d6e6e4c9a161f316bd8465a3b3
FoxTrot2254 <elischomer@gmail.com>
2024-01-27 23:52:16 -0500

    player rotation works

--- Assets/PlayerMovement/Playermove.cs
+++ Assets/PlayerMovement/Playermove.cs
@@ -1,9 +1,10 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Playermove : MonoBehaviour
 {
+    public Camera cam;
     public float moveSpeed;
     float speedX, speedY;
     Rigidbody2D Rb;
@@ -14,9 +15,13 @@ public class Playermove : MonoBehaviour
     [Range(0, 5)]
     public float sightDistance;
     public float checkInterval;
+    public Vector2 mousePos;
+    public Vector2 playerPos;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
+        cam = Camera.main;
         Rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         secondaryFog.localScale = new Vector2(sightDistance, sightDistance) * 10f;
@@ -26,24 +31,33 @@ public class Playermove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        speedX = Input.GetAxisRaw("Horizontal")* moveSpeed;
+        playerPos = transform.position;
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition); ;
+        CalculateAngleForAnim(playerPos, mousePos);
+        speedX = Input.GetAxisRaw("Horizontal") * moveSpeed;
         speedY = Input.GetAxisRaw("Vertical") * moveSpeed;
         Vector2 direction = new(speedX, speedY);
         Rb.velocity = direction;
+
+        float ang = AngleBetweenVector2(mousePos, transform.position);
+
 
         if (direction != Vector2.zero)
         {
             anim.SetBool("Walking", true);
 
-            float angle = Vector2.SignedAngle(Vector2.up, direction);
-            Quaternion newRot = Quaternion.Euler(0, 0, angle);
+            //float angle = Vector2.SignedAngle(Vector2.up, direction);
+            //Quaternion newRot = Quaternion.Euler(0, 0, angle);
+            //transform.rotation = newRot;
+
+            Quaternion newRot = Quaternion.Euler(0, 0, ang);
             transform.rotation = newRot;
         }
         else
         {
             anim.SetBool("Walking", false);
         }
-        
+
     }
     private IEnumerator CheckFogOfWar(float checkInterval)
     {
@@ -52,5 +66,18 @@ public class Playermove : MonoBehaviour
             Fog.MakeHole(transform.position, sightDistance);
             yield return new WaitForSeconds(checkInterval);
         }
+    }
+    private void CalculateAngleForAnim(Vector2 me, Vector2 target)
+    {
+        float angleBetween = AngleBetweenVector2(me, target);
+        Debug.Log(angleBetween);
+        player.transform.rotation = Quaternion.Euler(0, 0, angleBetween);
+
+    }
+    private float AngleBetweenVector2(Vector2 vec1, Vector2 vec2)
+    {
+        Vector2 diference = vec2 - vec1;
+        float sign = (vec2.y < vec1.y) ? -1.0f : 1.0f;
+        return Vector2.Angle(Vector2.right, diference) * sign;
     }
 }
