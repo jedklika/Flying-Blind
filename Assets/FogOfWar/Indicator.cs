fa0452349a91d0b17934f750e5c5516ba8f85aa5
jedklika <jedklika@gmail.com>
2024-03-05 18:31:06 -0500

    fog lights and doors

--- Assets/FogOfWar/Indicator.cs
+++ Assets/FogOfWar/Indicator.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Indicator : MonoBehaviour
 {
+    public Fog Fog;
+    public Transform secondaryFog;
+    [Range(0, 5)]
+    public float sightDistance;
+    public float checkInterval;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Fog = GameObject.Find("FogOfWar").GetComponent<Fog>();
+        secondaryFog.localScale = new Vector2(sightDistance, sightDistance) * 10f;
+        StartCoroutine(CheckFogOfWar(checkInterval));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private IEnumerator CheckFogOfWar(float checkInterval)
+    {
+        while (true)
+        {
+            Fog.MakeHole(transform.position, sightDistance);
+            yield return new WaitForSeconds(checkInterval);
+        }
     }
 }
